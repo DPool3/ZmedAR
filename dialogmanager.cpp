102f7bdc915d2f9465e6658c8772db54bff63ce2
DPool3 <danielpfeifer25@yahoo.de>
2021-05-04 13:01:45 +0200

    clean up of staged information.

--- dialogmanager.cpp
+++ dialogmanager.cpp
@@ -4,3 +4,24 @@ DialogManager::DialogManager()
 {
 
 }
+
+void DialogManager::callErrorDialog(std::string errorMsg){
+    ErrorDialog errd;
+    errd.setModal(true);
+    errd.setErrorText(errorMsg.c_str());
+    errd.exec();
+}
+
+void DialogManager::callErrorDialog(const char* errorMsg){
+    ErrorDialog errd;
+    errd.setModal(true);
+    errd.setErrorText(errorMsg);
+    errd.exec();
+}
+
+QString DialogManager::getPathFromFileSystem(){
+    FileSystem fsd;
+    fsd.setModal(true);
+    fsd.exec();
+    return fsd.getSelectedFile();
+}
--- dialogmanager.h
+++ dialogmanager.h
@@ -1,11 +1,18 @@
 #ifndef DIALOGMANAGER_H
 #define DIALOGMANAGER_H
 
+#include "errordialog.h"
+#include "filesystem.h"
 
 class DialogManager
 {
 public:
     DialogManager();
+
+    void callErrorDialog(std::string);
+    void callErrorDialog(const char*);
+    QString getPathFromFileSystem();
+
 };
 
 #endif // DIALOGMANAGER_H
